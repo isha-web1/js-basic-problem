3e26cde544a565398f27966ed606aef6356a828d
isha-web1 <isha.web1@gmail.com>
2023-02-04 20:19:42 +0600

    largestName added

---
+++ largestName.js
@@ -0,0 +1,18 @@
+/* একটা ফাংশন লিখবে। এই ফাংশনের নাম হবে bestFriend তারপর সেই ফাংশনে ইনপুট প্যারামিটার হিসেবে একটা array নিবে। সেই array এর মধ্যে তোমার সব ফ্রেন্ডের নাম থাকবে। এখন তোমার কাজ হচ্ছে যে ফ্রেন্ড এর নাম সবচেয়ে বড় সেই ফ্রেন্ড এর নাম রিটার্ন করে দেয়া। এই ক্ষেত্রে তুমি নামটা অর্থাৎ ফ্রেন্ডের নাম (স্ট্রিং) রিটার্ন করতে হবে।  */
+
+
+function bestFriend(names){
+    let max;
+    let maxNumber = names[0].length;
+    for(let i = 0; i < names.length; i++){
+        if(names[i].length>maxNumber){
+            maxNumber = names[i].length;
+            max = names[i];
+        }
+        
+    }
+    return max;
+}
+
+const bestFriendNames =['rabbi', 'shahadat', 'shahadat', 'nazim', 'ahsanhabib', 'hasib', 'rifat', 'shakib'];
+console.log(bestFriend(bestFriendNames));
